c1be7f0c57086c938d9b7de9d5bee0d9f83e7e9f
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2018-11-16 15:31:38 +0100

    Kuntz

--- src/main/java/exercise2/Student.java
+++ src/main/java/exercise2/Student.java
@@ -3,7 +3,12 @@ package exercise2;
 import java.time.LocalDate;
 
 public class Student extends Person {
-	public Student(String firstName, String lastName, LocalDate birthDate) {
+	public Student(String firstName, String lastName, LocalDate birthDate)
+	{
 		super(firstName, lastName, birthDate);
 	}
+
+
+
+
 }
